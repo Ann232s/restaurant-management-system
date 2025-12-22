73b4c70aa65c5686676840fb58301998f72a2770
Ann232s <anasta.razgonova@bk.ru>
2025-12-22 20:57:37 +0200

    Исходный код класса Recipe для рефакторинга

---
+++ Lab4_Refactoring/Recipe.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantSystem
+{
+    public class Ingredient
+    {
+        public Product Product { get; set; }
+        public decimal Quantity { get; set; }
+    }
+
+    public class Product
+    {
+        public decimal Price { get; set; }
+    }
+
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal CostPrice { get; private set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+
+        public void CalculateCostPrice()
+        {
+            CostPrice = 0;
+            foreach (var ingredient in Ingredients)
+            {
+                CostPrice += ingredient.Product.Price * ingredient.Quantity;
+            }
+        }
+
+        public void SetPriceWithMarkup(decimal markupPercent)
+        {
+            Price = CostPrice * (1 + markupPercent / 100);
+        }
+
+        public bool CanBeActivated()
+        {
+            return Ingredients.Count > 0 && CostPrice > 0;
+        }
+
+        public decimal GetPriceWithTax()
+        {
+            return CalculatePriceWithTax(Price, 0.2m);
+        }
+
+        private decimal CalculatePriceWithTax(decimal price, decimal taxRate)
+        {
+            return price * (1 + taxRate);
+        }
+
+        public decimal CalculateProfitPerServing(decimal sellingPrice, decimal taxRate)
+        {
+            // taxRate не используется в расчёте
+            return sellingPrice - CostPrice;
+        }
+
+        public void ApplyMarkupPercent(decimal markupPercent)
+        {
+            Price = CostPrice * (1 + markupPercent / 100);
+        }
+    }
+}
